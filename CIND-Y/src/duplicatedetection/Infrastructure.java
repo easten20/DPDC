08b61213ceac411c8f9d7ac0844d32bce3e2b484
easten <easten@hotmail.com>
2013-06-12 10:56:00 +0200

    read file

--- CIND-Y/src/duplicatedetection/Infrastructure.java
+++ CIND-Y/src/duplicatedetection/Infrastructure.java
@@ -1,5 +1,40 @@
 package duplicatedetection;
 
-public class Infrastructure {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Infrastructure {
+	String tableLocation = "D:\\university\\Data Profiling and Data Cleansing\\exercise4\\addresses.tsv";
+
+	public Infrastructure(String tableLocation)
+	{
+		this.tableLocation = tableLocation;
+	}
+	
+	
+	public void ReadFile(int topN) 
+	{
+		try
+		{
+			FileInputStream fileStream = new FileInputStream(this.tableLocation);
+			DataInputStream in = new DataInputStream(fileStream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			for (int i = 0 ; i<topN; i++)
+			{
+				String strLine = br.readLine();
+				String[] columns = strLine.split("\t");
+				System.out.println(Arrays.toString(columns));			
+			}
+			br.close();
+			fileStream.close();
+		}
+		catch (IOException ex)
+		{
+			System.out.println("read file failed: " + ex.getMessage());
+		}
+	}
 }
---
+++ CIND-Y/src/duplicatedetection/TestProgramm.java
@@ -0,0 +1,14 @@
+package duplicatedetection;
+
+public class TestProgramm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Infrastructure test = new Infrastructure("D:\\university\\Data Profiling and Data Cleansing\\exercise4\\addresses.tsv");
+		test.ReadFile(10);
+
+	}
+
+}
