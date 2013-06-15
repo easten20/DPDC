f62ef4ca986daa5396c338942018cd85b287ea55
easten <easten@hotmail.com>
2013-06-15 12:48:14 +0200

    add grouping and put the new value of grouping to new table...

--- CIND-Y/src/duplicatedetection/Infrastructure.java
+++ CIND-Y/src/duplicatedetection/Infrastructure.java
@@ -1,20 +1,99 @@
 package duplicatedetection;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Infrastructure {
-	String tableLocation;
+	String tableLocation;	
 
-	public Infrastructure(String tableLocation)
+	FileInputStream fileStream;
+	Map<Integer, Integer> mapPostiontoIndex = new HashMap<Integer, Integer>();
+	
+	public Infrastructure(String tableLocation) throws IOException
 	{
 		this.tableLocation = tableLocation;
+		fileStream = new FileInputStream(this.tableLocation);
+		this.MapIndexToPosition();
 	}
 	
+	public String[] GetRowByPosition(int stopPosition) throws IOException
+	{
+		FileInputStream fileStream = new FileInputStream(this.tableLocation);
+		DataInputStream in = new DataInputStream(fileStream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		int position = 0;
+		String strLine;
+		String[] columns = null;
+		while ((strLine = br.readLine()) != null)
+		{
+			if (stopPosition == position)
+			{
+				columns = strLine.split("\t");						
+				break;
+			}
+			position++;
+		}
+		br.close();		
+		return columns;
+	}
+	
+	public String[] GetRowByIndex(int index) throws IOException
+	{
+		FileInputStream fileStream = new FileInputStream(this.tableLocation);
+		DataInputStream in = new DataInputStream(fileStream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		int position = 0;
+		String strLine;
+		String[] columns = null;
+		int stopPosition = this.mapPostiontoIndex.get(index);
+		while ((strLine = br.readLine()) != null)
+		{
+			if (stopPosition == position)
+			{
+				columns = strLine.split("\t");						
+				break;
+			}
+			position++;
+		}
+		br.close();		
+		return columns;
+	}
+	
+	public String GetRowFullByIndex(int index) throws IOException
+	{
+		FileInputStream fileStream = new FileInputStream(this.tableLocation);
+		DataInputStream in = new DataInputStream(fileStream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));	
+		String strLine;		
+		int position = 0;
+		int stopPosition = this.mapPostiontoIndex.get(index);
+		while ((strLine = br.readLine()) != null)
+		{			
+			if (position == stopPosition)
+				break;
+			position++;
+		}
+		br.close();		
+		return strLine;
+	}
+	
+	public void dispose() throws IOException
+	{
+		fileStream.close();
+	}
 	
 	public void ReadFile(int topN) 
 	{
@@ -37,4 +116,95 @@ public class Infrastructure {
 			System.out.println("read file failed: " + ex.getMessage());
 		}
 	}
+	
+	public int GetFileLength() throws IOException 
+	{
+		int length = 0;		
+			FileInputStream fileStream = new FileInputStream(this.tableLocation);
+			DataInputStream in = new DataInputStream(fileStream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			while (br.readLine() != null)
+			{
+				length++;						
+			}
+			br.close();
+			fileStream.close();					
+			return length;
+	}
+	
+	public Set<String> GroupFile(int[] groupParameters) throws IOException
+	{		
+			Set<String> groupComb = new HashSet<String>(); 
+			FileInputStream fileStream = new FileInputStream(this.tableLocation);
+			DataInputStream in = new DataInputStream(fileStream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			while (null!=(strLine=br.readLine()))
+			{				
+				String[] columns = strLine.split("\t");
+				String strComb = ""; 
+				for (int i = 0; i < groupParameters.length; i++)
+				{
+					strComb += columns[groupParameters[i]];
+				}
+				groupComb.add(strComb);
+			}			
+			br.close();
+			fileStream.close();
+			return groupComb;			
+	}
+	
+	public HashMap<String, ArrayList<Integer>> GroupFileHashMap(int[] groupParameters) throws IOException
+	{		
+			HashMap<String, ArrayList<Integer>> hashMapComb = new HashMap<>(); 
+			FileInputStream fileStream = new FileInputStream(this.tableLocation);
+			DataInputStream in = new DataInputStream(fileStream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;						
+			while (null!=(strLine=br.readLine()))
+			{				
+				String[] columns = strLine.split("\t");
+				String strComb = ""; 
+				for (int i = 0; i < groupParameters.length; i++)				
+					strComb += columns[groupParameters[i]];
+				ArrayList<Integer> listIndexes = new ArrayList<Integer>();
+				if (hashMapComb.containsKey(strComb))				
+					listIndexes = hashMapComb.get(strComb);				
+				listIndexes.add(Integer.parseInt(columns[0]));				
+				hashMapComb.put(strComb, listIndexes);												
+			}			
+			br.close();
+			fileStream.close();
+			return hashMapComb;			
+	}
+	
+	private void MapIndexToPosition() throws IOException
+	{
+		FileInputStream fileStream = new FileInputStream(this.tableLocation);
+		DataInputStream in = new DataInputStream(fileStream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		int position = 0;
+		while (null!=(strLine=br.readLine()))
+		{				
+			String[] columns = strLine.split("\t");
+			this.mapPostiontoIndex.put(Integer.parseInt(columns[0]), position);
+			position++;
+		}			
+		br.close();
+		fileStream.close();		
+	}
+	
+	public void CreateFileBasedOnIndex(Set<Integer> indexes, String targetLocation) throws IOException
+	{							
+			BufferedWriter bufferedWriter = new BufferedWriter ( new FileWriter ( targetLocation ) );			
+		    for (Integer index: indexes)
+		    {
+		    	bufferedWriter.write(this.GetRowFullByIndex(index));
+		    	bufferedWriter.newLine();
+		    }
+		    bufferedWriter.flush();
+		    bufferedWriter.close();
+	}
+	
 }
--- CIND-Y/src/duplicatedetection/TestProgramm.java
+++ CIND-Y/src/duplicatedetection/TestProgramm.java
@@ -1,14 +1,62 @@
 package duplicatedetection;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestProgramm {
 
+	
+	/**
+	 * 
+	 * @author easten
+	 * to get columnInfo index please use ordinal 
+	 * for example:
+	 * columnInfo.id.ordinal() will return 0;
+	 */
+	public enum columnInfo 
+	{
+		id, 
+		culture, 
+		sex, 
+		age, 
+		date_of_birth, 
+		title, 
+		given_name, 
+		surname, 
+		state, 
+		suburb, 
+		postcode, 
+		street_number, 
+		address_1, 
+		address_2, 
+		phone_number
+	}
+	
 	/**
 	 * @param args
-	 */
+	 */	
 	public static void main(String[] args) {
-		Infrastructure test = new Infrastructure("C:\\DPDC_Exercise4\\addresses.tsv");
-		test.ReadFile(10);
-//test6666
+		try
+		{
+			Infrastructure test = new Infrastructure("C:\\DPDC_Exercise4\\addresses.tsv");
+			HashMap<String, ArrayList<Integer>> grouping = test.GroupFileHashMap(new int[] { columnInfo.culture.ordinal(), columnInfo.postcode.ordinal(),columnInfo.date_of_birth.ordinal() });
+			Set<Integer> groupIndex = new HashSet<Integer>();			
+			for (String key: grouping.keySet())
+			{							
+				ArrayList<Integer> group = grouping.get(key);			
+				groupIndex.add(group.get(0));								
+			}			
+			test.CreateFileBasedOnIndex(groupIndex, "C:\\DPDC_Exercise4\\test.tsv");									
+		}
+		catch (Exception ex)
+		{
+			System.out.println("read file failed: " + ex.getMessage());
+		}
 	}
 
 }
