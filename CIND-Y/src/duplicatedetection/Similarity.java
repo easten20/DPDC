f1db99db28b4cc0654461d2d6081a2a0f3925b06
Jaeyoon <jaeyoon.jung@student.hpi.uni-potsdam.de>
2013-06-18 01:02:49 +0200

    Similarity.java modified

--- CIND-Y/src/duplicatedetection/Similarity.java
+++ CIND-Y/src/duplicatedetection/Similarity.java
@@ -1,15 +1,32 @@
 package duplicatedetection;
 
+import com.wcohen.ss.MongeElkan;
+
+
+
 public class Similarity {
+	
+	static double threshold = 0.7;
 	
 	public Similarity()
 	{
 		
 	}
 	
-	private boolean CheckSimilarity(String value1, String value2)
+	private double ScoreOfSimilarity(String value1, String value2){
+		
+		MongeElkan mongeElkan = new MongeElkan();
+		
+		return mongeElkan.score(value1, value2); 		// 0 < scoreOfSimilarity < 1
+	}
+	
+	private boolean isSame(String value1, String value2)
 	{
-		boolean isSimilar = false;
-		return isSimilar;
+		MongeElkan mongeElkan = new MongeElkan();
+		
+		double scoreOfSimilarity = mongeElkan.score(value1, value2); // 0 < scoreOfSimilarity < 1
+		
+		if(scoreOfSimilarity > threshold) return true;
+		else return false;
 	}	
 }
